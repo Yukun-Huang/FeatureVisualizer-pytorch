a5589bffc24123972de0391cf704f320e18a3faa
hyk1996 <563837686@qq.com>
2019-09-18 16:33:18 +0800

    new commit

--- demo.py
+++ demo.py
@@ -1,35 +1,26 @@
 import torch
 from feature_visualizer import FeatureVisualizer
-from torchvision.utils import save_image
 
 ## Load Data
-img = torch.load('data/image.pt')   # torch.Size([8, 3, 288, 144])
-f = torch.load('data/feature.pt')   # torch.Size([8, 2048, 9, 5])
+img = torch.load('data/image.pt')   # torch.Size([4, 3,   256, 128])
+f = torch.load('data/feature.pt')   # torch.Size([4, 2048,  8,   4])
 
 ## Feature visualizer
 V = FeatureVisualizer(
     cmap_type='jet',
     reduce_type='mean',
-    upsample_size=(288, 144),
+    upsample_size=(256, 128),
     upsample_type='bilinear',
 )
 
-'''
 ## Visualize Feature
-FeatureVisualizer.show() and FeatureVisualizer.save() are not support to visualize RGB images.
-If you want to show rgb images, use 'save_image()' in torchvision.
-'''
-V.show(f)
-V.save(f, 'demo/feature.jpg')
+V.save_feature(f, save_path='demo/feature.jpg')
 
-save_image(img[0,:,:,:], 'demo/image.jpg')
+## Visualize Image
+""" If image data are processed by transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+you should set recover=True. """
+V.save_image(img, save_path='demo/image.jpg', recover=False)
 
-
-'''
 ## Visualize both Image and Feature
-If image data are processed by transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
-you should set norm=True.
-'''
-V.show_both(img, f, norm=False)
-V.save_both(img, f, 'demo/demo.jpg', norm=False)
+V.save_both(img, f, 'demo/demo.jpg', recover=False)
 
--- feature_visualizer.py
+++ feature_visualizer.py
@@ -2,13 +2,13 @@ import torch
 from torch.nn import functional as F
 from matplotlib import pyplot as plt
 import numpy as np
+from torchvision.utils import save_image
 
 
 class FeatureVisualizer:
 
     def __init__(self, upsample_size=(256, 256), cmap_type='jet', reduce_type='mean', upsample_type='bilinear'):
-        self.mean = np.array([0.485, 0.456, 0.406])
-        self.std = np.array([0.229, 0.224, 0.225])
+
         self.color_map = plt.get_cmap(cmap_type)
         self.reduce_type = reduce_type
         self.upsample_size = upsample_size
@@ -16,79 +16,85 @@ class FeatureVisualizer:
         self.height = upsample_size[0]
         self.width = upsample_size[1]
 
-    def _check(self, x):
-        ## Assert
-        assert len(x.shape) == 3 or len(x.shape) == 4, \
-            'Input should be 3D or 4D tensor.'
-        ## Reduce by batch, choose the first sample
-        if len(x.shape) == 4:
-            x = x[0,:,:,:]
-        return x.unsqueeze(dim=0)  # shape=[1, C, H, W]
+        self.mean = np.array([0.485, 0.456, 0.406])
+        self.std = np.array([0.229, 0.224, 0.225])
+        self.t_mean = torch.Tensor([[[[0.485]], [[0.456]], [[0.406]]]])
+        self.t_std = torch.Tensor([[[[0.229]], [[0.224]], [[0.225]]]])
 
-    def _norm(self, x):
+    def __check_dimention(self, x):
+        assert x.dim() == 3 or x.dim() == 4, 'Input should be 3D or 4D tensor.'
+
+    def __recover_numpy(self, x):
+        x = self.std * x + self.mean
+        x = x * 255.0
+        x = np.clip(x, 0, 255)
+        x = x.astype(np.uint8)
+        return x
+
+    def __recover_torch(self, x):
+        return x * self.t_std + self.t_mean
+
+    def __normalize(self, x):
         max_val, min_val = torch.max(x), torch.min(x)
         x = (x - min_val) / (max_val - min_val)
         return x
 
-    def _recover(self, inp):
-        inp = self.std * inp + self.mean
-        inp = inp * 255.0
-        inp = np.clip(inp, 0, 255)
-        inp = inp.astype(np.uint8)
-        return inp
+    def __transform_image(self, x, recover):
+        """
+        Transform image from torch.tensor to numpy.array.
+        """
+        # reduce by batch, choose the first sample
+        x = x[0].unsqueeze(dim=0) if x.dim() == 4 else x.unsqueeze(dim=0)
+        x = F.interpolate(x, size=self.upsample_size, mode=self.upsample_type, align_corners=False)
+        x = x.squeeze(dim=0).detach().cpu().numpy().transpose((1, 2, 0))
+        return self.__recover_numpy(x) if recover else x
 
-    def _transform(self, f):
-        f = self._check(f)
-        f = self._norm(f)  # normalize to [0, 1]
+    def __transform_feature(self, f):
+        """
+        Transform feature from torch.tensor to numpy.array.
+        """
+        # reduce by batch, choose the first sample
+        f = f[0].unsqueeze(dim=0) if f.dim() == 4 else f.unsqueeze(dim=0)
+        # normalize to [0, 1]
+        f = self.__normalize(f)
         if self.reduce_type == 'mean':
-            f = torch.mean(f, dim=1, keepdim=True)  # reduce by channel
+            f = f.mean(dim=1, keepdim=True)  # reduce by channel
             f = F.interpolate(f, size=self.upsample_size, mode=self.upsample_type, align_corners=False)
         else:
             raise NotImplementedError
         return f.squeeze().detach().cpu().numpy()
 
-    def _transform_rgb(self, x, norm):
-        x = self._check(x)
-        x = F.interpolate(x, size=self.upsample_size, mode=self.upsample_type, align_corners=False)
-        x = x.squeeze(dim=0).detach().cpu().numpy().transpose((1, 2, 0))
-        if norm:
-            x = self._recover(x)
-        return x
-
-    def _draw(self, x, ca):
+    def __draw(self, x, ca):
         # set x and y axis invisible
         ca.axes.get_xaxis().set_visible(False)
         ca.axes.get_yaxis().set_visible(False)
         # draw
         ca.imshow(x, cmap=self.color_map)
 
-    def show(self, f):
-        f = self._transform(f)
-        self._draw(f, plt.gca())
-        plt.show()
-        plt.close()
-
-    def save(self, f, save_path):
-        f = self._transform(f)
-        self._draw(f, plt.gca())
+    def save_feature(self, f, save_path, show=False):
+        self.__check_dimention(f)
+        f = self.__transform_feature(f.cpu())
+        self.__draw(f, plt.gca())
         plt.savefig(save_path, bbox_inches='tight', pad_inches=0.0)
+        if show:
+            plt.show()
         plt.close()
 
-    def show_both(self, img, f, norm=False):
+    def save_image(self, imgs, save_path, nrow=8, recover=False):
+        self.__check_dimention(imgs)
+        if recover:
+            imgs = self.__recover_torch(imgs)
+        save_image(imgs, save_path, nrow=nrow)
+
+    def save_both(self, img, f, save_path, recover=False, show=False):
         fig = plt.figure()
         ax0 = fig.add_subplot(121, title="image")
         ax1 = fig.add_subplot(122, title="feature")
-        self._draw(self._transform_rgb(img, norm), ax0)
-        self._draw(self._transform(f), ax1)
-        plt.show()
-        plt.close()
-
-    def save_both(self, img, f, save_path, norm=False):
-        fig = plt.figure()
-        ax0 = fig.add_subplot(121, title="image")
-        ax1 = fig.add_subplot(122, title="feature")
-        self._draw(self._transform_rgb(img, norm), ax0)
-        self._draw(self._transform(f), ax1)
+        self.__draw(self.__transform_image(img, recover), ax0)
+        self.__draw(self.__transform_feature(f), ax1)
         plt.savefig(save_path)
+        if show:
+            plt.show()
         plt.close()
+
 
